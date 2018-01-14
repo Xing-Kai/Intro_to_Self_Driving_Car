b5231097d0c23a979c427e129ecde5730fce3d16
Xing-Kai <xingkai414@gmail.com>
2018-01-14 15:27:12 +0800

    Update

--- 4_C++_Basics/Project_Translate_Python_to_C++/udacity_cpp_histogram_filter_project_original/helpers.cpp
+++ 4_C++_Basics/Project_Translate_Python_to_C++/udacity_cpp_histogram_filter_project_original/helpers.cpp
@@ -31,27 +31,13 @@ using namespace std;
     @return - a new normalized two dimensional grid where the sum of 
     	   all probabilities is equal to one.
 */
-vector< vector<float> > normalize(vector< vector <float> > &grid) {
+vector< vector<float> > normalize(vector< vector <float> > grid) {
 	
+	vector< vector<float> > newGrid;
+
 	// todo - your code here
-	float total = 0.0;
-	int height = grid.size();
-	int width = grid[0].size();
-	int i, j;
 
-	for (i = 0; i < height; i++){
-		for (j=0; j< width; j++){
-			total += grid[i][j];
-		}
-	}
-
-	for (i = 0; i < height; i++) {
-		for (j=0; j< width; j++) {
-			grid[i][j] = grid[i][j] / total;
-		}
-	}
-
-	return grid;
+	return newGrid;
 }
 
 /**
@@ -87,57 +73,11 @@ vector< vector<float> > normalize(vector< vector <float> > &grid) {
     @return - a new normalized two dimensional grid where probability 
     	   has been blurred.
 */
-vector < vector <float> > blur(vector < vector < float> > &grid, float blurring) {
+vector < vector <float> > blur(vector < vector < float> > grid, float blurring) {
 
+	vector < vector <float> > newGrid;
+	
 	// your code here
-	int height = grid.size();
-	int width = grid[0].size();
-
-	vector< vector <float> > newGrid;
-  	newGrid.reserve(height);
-    vector<float> newRow;
-    newRow.reserve(width);
-
-	static float center = 1.0 - blurring;
-	static float corner = blurring / 12.0;
-	static float adjacent = blurring / 6.0;
-  	// OPTIMIZATION: window, DX and  DY variables have the 
-    // same value each time the function is run.
-  	// It's very inefficient to recalculate the vectors
-    // every time the function runs. 
-    // Define and declare window, DX, and DY using the
-    // bracket syntax: vector<int> foo = {1, 2, 3, 4} 
-    // instead of calculating these vectors with for loops 
-    // and push back
-    static vector < vector <float> > window = {{corner, adjacent, corner}, {adjacent, center, adjacent}, {corner, adjacent, corner}};
-	static vector <int> DX = {-1, 0, 1};
-	static vector <int> DY = {-1, 0, 1};
-
-	int ii;
-	int jj;
-	int new_i;
-	int new_j;
-	float multiplier;
-
-	//Initialize newGrid
-  	newRow.assign(width, 0);
-  	for (int i=0; i<height; i++) {
-      newGrid.push_back(newRow);
-	}
-
-	for (int i=0; i< height; i++ ) {
-		for (int j=0; j<width; j++ ) {
-			for (ii=0; ii<3; ii++) {
-				for (jj=0; jj<3; jj++) {
-					new_i = (i + DY[ii] + height) % height;
-					new_j = (j + DX[jj] + width) % width;
-					multiplier = window[ii][jj];
-					newGrid[new_i][new_j] += grid[i][j] * multiplier;
-				}
-			}
-		}
-	}
-
 
 	return normalize(newGrid);
 }
