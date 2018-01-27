bdffa2b8a0429611b51ff36ad8aae67077102a5c
Xing-Kai <xingkai414@gmail.com>
2018-01-27 21:43:00 +0800

    Updating

--- 6_Navigating_Data_Structures/Project_Implement_Route_Planner/student_code.py
+++ 6_Navigating_Data_Structures/Project_Implement_Route_Planner/student_code.py
@@ -1,3 +1,57 @@
 def shortest_path(M,start,goal):
+
+	## initialise 
+	frontier = set()
+	known = set()
+	path = []
+
+	frontier.append(start)
+
+	## initialise all node 
+	## node = {"node_id": ;
+	## 		"x_coordinate": ;
+	##		"y_coornidate": ;
+	##		"g": ;
+	##		"h" ;
+	##		"f": ;
+	##		"neighbors": ;
+	##		"parent": }
+
+	wihle frontier != ():
+		## irerate node in frontier
+		for i in frontier:
+			## iterate neighbor node of i
+			for j in i.neighbors:
+				## update g(i), f(i)
+				g(j) = g(i) + w(ij) ## w(ij) is the distance between i and j
+				f(j) = g(j) + h(j) ## h(j) is the hypothesis distance between node j and goal
+				## update j's parent node to i
+				j.parent = i
+				frontier.append(j)
+			frontier.remove(i)
+
+		## choose the minimal f(i) in frontier 
+
+		## if the i with minimal f(i) is the goal, break the while loop
+		if i == goal
+			break
+		## or start searching again
+
+
+	## if find a shortest path to goal, then trace back to start, making the path
+	if i == goal:
+		path.append(goal)
+		path.append(goal.parent)
+		path.append(goal.parent.parent)
+	### until to the start node
+
+	## if the frontier is empty, yet the goal hasn't been reached
+	if frontier == [] and i != goal:
+		return "path not find"
+
+
+
+
+
     print("shortest path called")
     return
