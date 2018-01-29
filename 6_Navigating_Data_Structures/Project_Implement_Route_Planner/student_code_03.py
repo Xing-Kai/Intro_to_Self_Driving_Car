2aab76dc5897d85f12b8b5c7f0bb86a54b119273
Xing-Kai <xingkai414@gmail.com>
2018-01-29 20:28:30 +0800

    Updating

--- 6_Navigating_Data_Structures/Project_Implement_Route_Planner/student_code_03.py
+++ 6_Navigating_Data_Structures/Project_Implement_Route_Planner/student_code_03.py
@@ -4,6 +4,7 @@
 '''
 
 import math
+import heapq
 
 def shortest_path(M,start,goal):
 
@@ -29,6 +30,7 @@ def shortest_path(M,start,goal):
 
 	## initialize 
 	frontier = set()
+	frontier_heap = []
 	known = set()
 	path = []
 
@@ -45,68 +47,41 @@ def shortest_path(M,start,goal):
 
 	## add start to frontier
 	frontier.add(start)
+	## add start to priority queue frontier_heap
+	heapq.heappush(frontier_heap, (node[start]['f'], start))
 
-	## choose the node with minimal f in frontier
-	def choose_search_node(frontier):
-		## get the f values of i in frontier
-		f_frontier = {}
-		for i in frontier:
-			f_frontier[i] = node[i]['f']
-		## return the node number that has minimal f
-		return min(f_frontier, key=f_frontier.get)
+	## search for shortest path
+	while frontier != ():
+		## choose search node i with minimal f in frontier as the searching node
+		i = heapq.heappop(frontier_heap)[1]
 
-	## search the neighbors of node i with minimal f, updating g and f of it's neighbors
-	## adding neighbors to frontier, remove i from frontier to known
-	def search(i):
+		## search the neighbors of node i, update  it's neighbors
 		for j in M.roads[i]:
 			## choose neighbors that is not in known
 			if j not in known:
-				## if j hasn't been searched before, initialize j
-				if j not in frontier:
+				## initialize or update node j
+				if (j not in frontier) or ((node[i]['g'] + distance(i, j) + distance(j, goal)) < node[j]['f']):
 					node[j] = {'g': node[i]['g'] + distance(i, j), 
 								'h': distance(j, goal), 
 								'f': node[i]['g'] + distance(i, j) + distance(j, goal), 
 								'parent': i }
 
-				## if j has been searched, update node j if f of j is getting smaller
-				else:
-					if (node[i]['g'] + distance(i, j) + distance(j, goal)) < node[j]['f']:
-						node[j] = {'g': node[i]['g'] + distance(i, j), 
-									'h': distance(j, goal), 
-									'f': node[i]['g'] + distance(i, j) + distance(j, goal), 
-									'parent': i }
-
-				## add j to frontier
-				frontier.add(j)
+					## add j to frontier
+					frontier.add(j)
+					## add j to priority queue frontier_heap
+					heapq.heappush(frontier_heap, (node[j]['f'], j))
 
 		## move i from frontier to known
 		frontier.remove(i)
 		known.add(i)
 
-		return frontier, known
-
-
-	while frontier != ():
-		## choose search node in frontier
-		i = choose_search_node(frontier)
-
-		## search in i's neighbors, updating frontier and known
-		frontier, known = search(i)
-
-		## if goal is in frontier, and goal has the minimal f, then the path is found, break the loop
+		## if goal has been searched and moved to known, then the path is found, contruct the path and break the loop
 		if goal in known:
+			i = goal
+			while i:
+				path.append(i)
+				i = node[i]['parent']
+			path.reverse()
 			break
 
-
-	## if find a shortest path to goal, then trace back to start, return the path
-	if goal in known:
-		i = goal
-		while i:
-			path.append(i)
-			i = node[i]['parent']
-		path.reverse()
-		return path
-
-	## if the frontier is empty, yet the goal hasn't been reached, return path not found
-	if frontier == [] and i != goal:
-		return "path not find"
+	return path
